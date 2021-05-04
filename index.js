8d8c7b029a480ad4a13c10cc19928af7c69e5c2a
Marc Steele <marcsteele2014@gmail.com>
2021-05-04 18:00:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 const num1 = 10
 const num2 = 6.2
-// const num3 =
-// const num4 =
+const num3 = 24
+const num4 =5
 const multipily = num1*num2
 const random = Math.floor(Math.random()*100) +1
+const mod = num3%num4
