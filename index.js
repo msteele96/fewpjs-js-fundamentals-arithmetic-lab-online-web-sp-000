8fffb71ac0b14547244e7b70c230af4efbc62ef5
Marc Steele <marcsteele2014@gmail.com>
2021-05-04 17:59:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,5 @@ const num1 = 10
 const num2 = 6.2
 // const num3 =
 // const num4 =
-
-function multiply(num1, num2) {
-  num1*num2
-};
-
-function random() {
-  return Math.floor(Math.random()*10) + 1
-};
+const multipily = num1*num2
+const random = Math.floor(Math.random()*100)
